c7dfbaffb5cf55ddf17737e77b988f656d999c73
nikoo978 <160965429+nikoo978@users.noreply.github.com>
2026-09-03 02:12:06 -0300

    V1.061: versiona lote 4 de biblioteca de ejercicios

---
+++ supabase/migrations/20260903050458_gf_v1061_exercise_library_batch_4.sql
@@ -0,0 +1,319 @@
+-- GymFlow V.1.061 - biblioteca de ejercicios lote 4/4
+select private.gf_seed_exercise_library($LIB$
+1075 - EZ Bar curl predicador - agarre invertido
+1076 - Con mancuerna a una mano curl de muñeca
+1077 - Con mancuerna curl de muñeca - agarre neutro
+1078 - Con mancuerna a una mano curl de muñeca Over Bench - Neutral
+1079 - Con mancuerna a una mano invertido curl predicador
+1080 - Con mancuerna a una mano invertido Bench curl de muñeca
+1082 - Con mancuerna sentado a una mano curl de muñeca
+1084 - En polea invertido curl predicador
+1085 - Con mancuerna invertido curl concentrado
+1089 - En polea curl de muñeca - agarre invertido
+1090 - En polea a una mano invertido curl
+1091 - Con mancuerna sentado curl de muñeca - agarre neutro
+1092 - Con barra invertido curl de muñeca
+1093 - Con mancuerna invertido curl de muñeca
+1094 - En Smith en máquina curl de muñeca
+1095 - Con mancuerna alternado invertido curl
+1097 - En polea Behind the espalda curl de muñeca
+1098 - Con mancuerna curl de bíceps - agarre invertido
+1099 - Con mancuerna a una mano invertido curl
+1100 - En Smith en máquina Behind the espalda curl de muñeca
+1101 - Cadera extensión - de pie
+1102 - En máquina Single-Prensa de piernas
+1103 - Con mancuerna a una mano Elevaciones frontales
+1104 - En Smith en máquina posterior press de hombros
+1106 - En polea Vuelos posteriores
+1107 - Con mancuerna sentado Bent-Over Vuelos posteriores
+1108 - Con mancuerna Bent-Over Vuelos posteriores
+1109 - Con barra posterior press militar
+1110 - Con barra sentado Elevaciones frontales
+1111 - Con mancuerna sentado alternado press de hombros
+1112 - Con mancuerna a una mano Elevaciones frontales - sentado
+1113 - Con mancuerna alternado Bent-Over Vuelos posteriores
+1114 - Con mancuerna a una mano Hammer press de hombros
+1115 - Weight con disco High Elevaciones frontales
+1116 - Con mancuerna a una mano Bent-Over Vuelos posteriores
+1117 - Con barra High Elevaciones frontales
+1118 - Con mancuerna alternado press de hombros
+1119 - En polea sobre la cabeza elevación - boca arriba
+1120 - Con mancuerna sentado alternado press Arnold
+1121 - Con mancuerna alternado Vuelos laterales
+1122 - Con mancuerna press Arnold
+1123 - Con mancuerna sentado a una mano press Arnold
+1124 - Con barra sentado High Elevaciones frontales
+1125 - Con mancuerna alternado Vuelos posteriores
+1126 - Con mancuerna alternado press Arnold
+1127 - En polea a una mano Vuelos laterales
+1128 - Con barra hombros tirón - boca abajo
+1129 - Con mancuerna a una mano Vuelos posteriores
+1130 - Con mancuerna a una mano press Arnold
+1132 - Con mancuerna Vuelos posteriores - boca abajo
+1133 - En polea a una mano Vuelos posteriores
+1135 - Con barra posterior Delt remo to Neck
+1136 - Con mancuerna sentado a una mano Vuelos laterales
+1137 - Con mancuerna alternado Vuelos posteriores - boca abajo
+1138 - En Smith en máquina press de hombros
+1139 - Con barra alternado press
+1140 - Con mancuerna sentado Elevaciones frontales
+1141 - Con mancuerna a una mano Vuelos posteriores
+1143 - Con barra sentado press
+1144 - Con mancuerna sentado alternado Elevaciones frontales
+1145 - Con mancuerna sentado alternado Vuelos posteriores
+1146 - Con mancuerna a una mano press de hombros
+1147 - Con mancuerna sentado press - Palms In
+1148 - Con mancuerna sentado a una mano Elevaciones frontales
+1149 - Con mancuerna a una mano Bent-Over Vuelos posteriores
+1150 - Con mancuerna alternado press
+1151 - Con mancuerna Dublin press
+1152 - Con mancuerna alternado press - Palms In
+1153 - Con barra press on Fitball
+1154 - Con barra alternado frontal and espalda press
+1155 - Con mancuerna sentado Dublin press
+1156 - En polea Remo al mentón - boca arriba
+1157 - En polea Remo sentado to Neck
+1158 - En polea Encogimiento de hombros - boca arriba
+1159 - En polea sentado Encogimiento de hombros
+1161 - Con mancuerna alternado Extensión de tríceps
+1162 - Con mancuerna alternado Kickback
+1163 - Con barra posterior Extensión de tríceps
+1164 - En polea tríceps Kickback
+1165 - Con barra agarre cerrado press
+1166 - Con mancuerna press - agarre cerrado
+1168 - Fitball con mancuerna Kickback
+1170 - En polea High Pulley Extensión de tríceps
+1171 - En polea con soga High Pulley Extensión de tríceps
+1173 - En polea con soga inclinado Extensión de tríceps
+1174 - Con barra press inclinado - agarre cerrado
+1175 - Con mancuerna inclinado Extensión de tríceps
+1176 - Con barra boca arriba invertido extensión
+1177 - En polea con soga Extensión de tríceps - boca arriba
+1178 - Con mancuerna Extensión de tríceps - boca arriba
+1179 - En polea a una mano Extensión de tríceps
+1180 - En Smith en máquina invertido press banca - agarre cerrado
+1181 - En Smith en máquina inclinado Extensión de tríceps
+1182 - En polea a una mano tríceps Kickback - agarre invertido
+1183 - EZ Bar press francés - agarre invertido
+1184 - Con mancuerna a una mano Extensión de tríceps - agarre invertido
+1185 - En polea a una mano Extensión de tríceps
+1186 - EZ Bar sentado agarre invertido press francés
+1187 - Con barra Rompecráneos - agarre invertido
+1188 - En polea a una mano Extensión de tríceps en polea - agarre invertido
+1189 - A una pierna Bench Dip
+1190 - Three Bench Dip
+1191 - Con mancuerna Rotational press banca
+1192 - En polea tríceps Kickback
+1193 - Weighted Three Bench Dip
+1194 - Weighted tríceps Dip
+1195 - Con barra Split salto
+1197 - Con mancuerna Peso muerto piernas rígidas on Bench
+1198 - Con mancuerna femoral curl
+1199 - Sentadilla hack - invertido Position
+1200 - En máquina Single-curl femoral
+1201 - Con barra Peso muerto piernas rígidas - Bench
+1202 - Con barra 1/4 Sentadilla
+1203 - Con mancuerna Wall Sentadilla
+1204 - En Smith en máquina Sentadilla frontal
+1205 - Fitball Wall Sentadilla
+1206 - A una pierna Sentadilla
+1207 - Sissy Sentadilla
+1208 - Con mancuerna a una pierna Bench Sentadilla
+1209 - Bodyweight Wall Sentadilla
+1210 - Con mancuerna Sentadilla con salto
+1211 - Fitball Weighted Wall Sentadilla
+1212 - Con mancuerna a una pierna Sentadilla
+1213 - En Smith en máquina Bench Sentadilla
+1215 - Bodyweight Estocada
+1216 - A una pierna Sentadilla hack
+1217 - En Smith en máquina Zercher Sentadilla
+1218 - Con barra declinado Bench Estocada
+1219 - Bodyweight posterior Estocada
+1220 - Bodyweight Estocada lateral
+1221 - Con barra 1/2 Sentadilla
+1222 - Bodyweight Estocadas caminando
+1223 - Con barra Deep Sentadilla
+1224 - Con mancuerna declinado Bench Estocada
+1225 - Bodyweight Subida al banco
+1226 - Con mancuerna Split salto
+1227 - Bodyweight Elevación de gemelos
+1228 - En polea Elevación de gemelos
+1229 - Fitball Elevación de gemelos
+1230 - Con barra Elevación de gemelos
+1232 - Hack Elevación de gemelos
+1233 - Hack a una pierna Elevación de gemelos
+1234 - En polea a una pierna Elevación de gemelos
+1235 - A una pierna Donkey Elevación de gemelos
+1236 - A una pierna Elevación de gemelos
+1237 - En máquina sentado a una pierna Elevación de gemelos
+1238 - Con mancuerna a una pierna Elevación de gemelos
+1239 - En Smith en máquina a una pierna Elevación de gemelos
+1241 - Con mancuerna sentado Elevación de gemelos
+1242 - En Smith en máquina a una pierna Elevación de gemelos
+1243 - En Smith en máquina Toe elevación
+1244 - En polea con soga Jalón al pecho
+1245 - En polea con soga Dominadas
+1246 - En máquina Jalón al pecho - agarre invertido
+1247 - Dominadas supinas - agarre cerrado
+1248 - En máquina invertido Jalón al pecho - agarre cerrado
+1249 - En máquina curl de bíceps - agarre invertido
+1251 - En máquina Prensa de piernas - Wide Stance
+1253 - En máquina Single-Prensa de piernas
+1254 - En máquina gemelos press
+1255 - En máquina Single-Prensa de piernas
+1256 - En máquina Single-Extensión de cuádriceps
+1257 - En máquina Single-curl femoral
+1258 - En máquina arrodillado curl femoral
+1259 - En máquina espalda extensión
+1260 - En máquina Deltoid elevación
+1261 - En máquina Assisted Dip
+1262 - En máquina Assisted Dominadas
+1263 - En máquina Assisted Dominadas supinas
+1264 - En máquina Assisted Dominadas - Hammer Grip
+1265 - En máquina Unilateral remo
+1266 - En máquina Vertical remo - agarre cerrado
+1267 - En máquina Vertical remo - agarre invertido
+1268 - En máquina Jalón al pecho
+1269 - En máquina Jalón al pecho - agarre invertido
+1270 - En máquina Inner press de pecho
+1273 - Parallel Bar Elevación de piernas
+1274 - En máquina Iso lateral High remo
+1276 - En máquina a una mano Pulldown
+1277 - En polea sentado sobre la cabeza curl
+1278 - En polea Bent-Over lateral Pulley
+1279 - En polea declinado Aperturas de pecho
+1280 - En polea declinado a una mano Aperturas de pecho
+1282 - En polea a una mano curl predicador
+1283 - En polea a una mano curl predicador - invertido
+1284 - En polea inclinado Aperturas - Fitball
+1285 - En polea press de pecho - boca arriba Fitball
+1286 - En polea inclinado press - Fitball
+1287 - En polea a una mano Aperturas - Fitball
+1288 - En polea a una mano press - Fitball
+1289 - En polea a una mano inclinado Aperturas - Fitball
+1290 - En polea a una mano inclinado press - Fitball
+1291 - En polea Dual sobre la cabeza curl - Fitball
+1292 - En polea invertido Crossover
+1293 - En polea con soga curl predicador - Hammer
+1294 - En polea con soga a una mano curl predicador - Hammer
+1295 - En polea V-Bar remo
+1299 - EZ Bar curl predicador - agarre cerrado
+1301 - En Smith en máquina Sentadilla frontal
+1303 - En Smith en máquina press banca - agarre ancho
+1304 - En Smith en máquina press de hombros - agarre cerrado
+1305 - En Smith en máquina press de hombros - agarre ancho
+1306 - En Smith en máquina press declinado - agarre cerrado
+1307 - En Smith en máquina press declinado - agarre invertido
+1308 - En Smith en máquina press declinado - agarre ancho
+1309 - En máquina Prensa de piernas - Wide Stance
+1310 - En máquina a una pierna gemelos press
+1311 - En máquina T Bar invertido remo
+1313 - En polea pierna Kickback
+1314 - Dragon Flag
+1315 - Con mancuerna curl araña
+1316 - EZ Bar press francés
+1317 - Con kettlebell a una mano Floor press
+1318 - Con mancuerna a una mano press Arnold - Fitball
+1319 - Con mancuerna alternado press Arnold - Fitball
+1320 - Con mancuerna sentado curl de muñeca
+1321 - Con mancuerna de pie alternado Vuelos laterales
+1322 - Con mancuerna sentado Elevaciones frontales - Hammer
+1323 - Con mancuerna alternado Elevaciones frontales
+1325 - Con mancuerna invertido curl araña
+1326 - Con mancuerna a una mano invertido curl araña
+1327 - Con barra declinado Pullover
+1328 - Con mancuerna a una mano Elevaciones frontales
+1329 - Con mancuerna a una mano posterior remo - boca abajo
+1330 - Con mancuerna posterior Delt remo
+1331 - Con mancuerna a una mano press - agarre invertido
+1332 - Con mancuerna sentado Bent-Over a una mano Vuelos posteriores
+1334 - En polea a una mano Extensión de tríceps en polea
+1335 - En polea a una mano curl concentrado
+1336 - Pelota medicinal Throw - Fitball
+1337 - Con banda Remo al mentón
+1338 - Dead Bug
+1339 - Burpee
+1340 - Inchworm
+1341 - Sentadilla dividida
+1342 - Straight Arm Plancha
+1343 - Arrodillado Thoracic Mobility
+1344 - Lateral-acostado Thoracic rotación
+1345 - Con mancuerna Farmers Carry
+1346 - Con mancuerna Sentadilla búlgara
+1347 - Jumping Jacks
+1348 - Con barra Landmine Sentadilla
+1349 - Con barra Landmine arrodillado a una mano press
+1350 - Con barra Landmine Remo a una mano
+1351 - Con barra Landmine Double Arm remo
+1352 - Puente de glúteos March
+1353 - Bird Dog Elbow to rodilla
+1355 - Plancha with alternado rodilla Drive
+1356 - Walking High Knees
+1357 - Walking Quadricep estiramiento
+1358 - Lateral Plancha Abducción de cadera
+1359 - Lateral Plancha Abdominal corto
+1360 - A una pierna Wall Sentadilla
+1361 - Lateral-acostado Abducción de cadera
+1362 - Inchworm empuje-Up
+1363 - Walking Inchworm empuje-Up
+1364 - Thoracic Spine extensión - Foam Roller
+1365 - Con mancuerna empuje press
+1366 - Con mancuerna Sentadilla to press de hombros
+1368 - Con mancuerna Sentadilla curl to press de hombros
+1369 - Con mancuerna Renegade remo
+1370 - Empuje-up to Renegade remo
+1371 - High rodilla Skip
+1372 - Lateral High rodilla Skip
+1373 - Straight pierna Walk
+1374 - Child Pose lateral estiramiento
+1375 - Arrodillado Warrior Estocada
+1376 - Warrior High Estocada
+1377 - Estocada hacia atrás with rotación
+1378 - Con mancuerna 3 Way hombros elevación
+1379 - Con mancuerna curl martillo to press
+1380 - Con kettlebell Suitcase Carry
+1381 - Con kettlebell Farmer's Carry
+1382 - Con mancuerna Wall Sentadilla Hold
+1383 - Diagonal con disco elevación
+1384 - Weight con disco Wall Sit
+1385 - Con kettlebell Swing
+1387 - Heel Walking
+1388 - Salto al cajóns
+1389 - A una pierna Salto al cajón
+1393 - Plyo Box Subida al bancos
+1394 - Salto al cajón Tap Overs
+1395 - Burpee Salto al cajón Overs
+1396 - Lateral Salto al cajón Overs
+1397 - Sentado Salto al cajóns
+1398 - Trap Bar Peso muerto
+1399 - Trap Bar Sentadilla
+1400 - Trap Bar Farmer’s Walk
+1401 - Trap Bar Encogimiento de hombross
+1402 - Trap Bar Estocadas
+1403 - Trap Bar Remo inclinados
+1404 - Trap Bar Sentadilla con salto
+1405 - Trap Bar sobre la cabeza press
+1406 - Trap Bar Peso muerto rumano
+1408 - Depth Jumps
+1409 - Arrodillado salto to Salto al cajón
+1410 - Depth salto to Tuck salto
+1412 - Pelota medicinal Salto al cajón
+1413 - Empuje-Up to Salto al cajón
+1414 - Depth Salto al cajóns
+1415 - Staggered Stance Salto al cajón
+1416 - Broad salto to Salto al cajón
+1417 - Weighted Salto al cajón
+1419 - Frontal to espalda pierna Swings
+1420 - Lateral to lateral pierna Swings
+1421 - Carioca
+1422 - Half arrodillado Psoas estiramiento
+1423 - Cat Cow estiramiento
+1424 - High rodilla Walk External cadera rotación
+1425 - Spiderman Estocada
+1426 - Bear Crawl
+1427 - Plancha with hombros Taps
+1429 - Estocada hacia adelante with rotación
+1430 - Pendlay remo
+1432 - Bodyweight Sentadilla sumo
+1433 - Con mancuerna Peso muerto rumano
+$LIB$);
